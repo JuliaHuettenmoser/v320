458eecf1502d7cda2beed121ee33372c3eaf08df
JuliaHuettenmoser <112378948+JuliaHuettenmoser@users.noreply.github.com>
2023-03-12 23:15:49 +0100

    ha 13.03.23

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using Ninject.Activation;
+using System.Globalization;
+
+
+namespace StringCalculator
+{
+    public class Add
+    {
+        //public int Addition(string numberString)
+        //{
+        //    int res = 0;
+        //    if (numberString.Length < 0)
+        //    {
+        //        return res;
+        //    }
+        //    string[] numbers = numberString.Split(',');
+        //        foreach (string strNumber in numbers)
+        //        {
+        //            int number = int.Parse(strNumber);
+        //            res += number;
+        //        }
+        //        return res;
+
+        //}
+
+        public int Addition(string numberString)
+        {
+            int res = 0;
+            String[] numbers = numberString.Split(',');
+
+            foreach(string strNumber in numbers)
+            {
+                int number = Convert.ToInt32(strNumber);
+                res += number;
+            }
+            return res;
+        }
+    }
+}
